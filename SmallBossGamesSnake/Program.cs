f4fe6ec47e90e687dc723dd906d0bfd437f092a5
Алексей Гардер <aleksey_garder@outlook.com>
2023-10-18 20:57:10 +0700

    Separate cross-platform code

--- SmallBossGamesSnake/Program.cs
+++ SmallBossGamesSnake/Program.cs
@@ -1,5 +1,4 @@
 ﻿using Avalonia;
-using Avalonia.ReactiveUI;
 using System;
 
 namespace AvaloniaNativeApplication1
@@ -17,7 +16,6 @@ namespace AvaloniaNativeApplication1
         public static AppBuilder BuildAvaloniaApp()
             => AppBuilder.Configure<App>()
                 .UsePlatformDetect()
-                .LogToTrace()
-                .UseReactiveUI();
+                .LogToTrace();
     }
 }
