f6a603608a528cc47c636422d53193a754911888
Алексей Гардер <aleksey_garder@outlook.com>
2025-07-09 21:48:49 +0700

    Cleanup

--- SmallBossGames.Snake/ViewModels/MainViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace SmallBossGames.Snake.ViewModels
-{
-    public class MainViewModel : ObservableObject
-    {
-        public string Greeting => "Welcome to Avalonia!";
-    }
-}
